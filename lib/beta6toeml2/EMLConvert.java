8738a469110a48a5ece8ad851f21e637aef56f08
Dan Higgins <higgins@nceas.ucsb.edu>
2003-03-20 00:53:16 +0000

    updates with documentation

--- lib/beta6toeml2/EMLConvert.java
+++ lib/beta6toeml2/EMLConvert.java
@@ -1,3 +1,39 @@
+/**
+ *  '$RCSfile: EMLConvert.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: higgins $'
+ *     '$Date: 2003-03-20 00:53:16 $'
+ * '$Revision: 1.2 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * The purpose of this class is to transform an emlbeta6 package to an eml2 document.
+ * This process is done using two succesive xslt transformations.
+ * 
+ * The 'main' class runs the transformation from the command line. The path to the 
+ * top level beta6 dataset module must be passed on the command line. All the associated 
+ * modules in the package are assumed to be in the same directory and the file names are
+ * the ids used in the beta6 triples (e.g. "higgins.232.3")
+ */ 
+
 import javax.xml.transform.*;
 import javax.xml.transform.sax.*;
 import javax.xml.transform.dom.*;
--- lib/beta6toeml2/MyURIResolver.java
+++ lib/beta6toeml2/MyURIResolver.java
@@ -1,3 +1,39 @@
+/**
+ *  '$RCSfile: MyURIResolver.java,v $'
+ *  Copyright: 2000 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: @authors@
+ *    Release: @release@
+ *
+ *   '$Author: higgins $'
+ *     '$Date: 2003-03-20 00:53:16 $'
+ * '$Revision: 1.2 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+  The purpose of this class is to provide a special loader for use in xalan xslt transformations.
+  It is used to remove the DOCTYPE elements from xml files loaded using the xslt 'document()' command.
+  Otherwise, the xalan transformation engine tries to load SystemIDa in certain documents, and these
+  are not necessarily correct/resolvable (especially since Morpho always uses the PublicID internally)
+  This class extends the Trax URIResolver interface. Besides removing DOCTYPE elements, the class also
+  provides for resolving the location of xsl/xml docs in one location and other documents 
+  (like emlbeta6 modules) in a separate location.
+*/ 
+ 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory; 
 import javax.xml.transform.TransformerException;
@@ -13,32 +49,46 @@ import java.io.*;
 
 public class MyURIResolver implements URIResolver{   
 
-  static String baseDefault = "./"; 
+  /*
+   * used when the 'base' parameter for the 'resolve' method is null
+   */
+  static String baseDefault = "./";
+  
+  /*
+   * used for non-xsl/xml documents; i.e. beta6 eml modules.
+   * this is basically the path to datafiles
+   */
   static String dataDefault = ""; 
 
+  /*
+   * method for setting the dataDefault param from another method
+   */
   static public void setDataDefault(String s) {
     dataDefault = s;
   }
  
+ /*
+  * single method required by URIResolver interface
+  */
  public Source resolve(String href, String base) throws TransformerException { 
   URL u = null;
   URL uDefault = null;
   try{  
     if ((base!=null)&&(isXMLXSL(href))) {
-     System.out.println("base: "+base+ " --href: "+href);
+//     System.out.println("base: "+base+ " --href: "+href);
      URL context = new URL(base); 
       baseDefault = base;
       u = new URL(context, href);
     } else if (isXMLXSL(href)) {
-      System.out.println("baseDefault: "+baseDefault+ " --href: "+href);
+//      System.out.println("baseDefault: "+baseDefault+ " --href: "+href);
       uDefault = new URL(baseDefault);
       u = new URL(uDefault, href); 
     } else {
-      System.out.println("dataDefault: "+dataDefault+ " --href: "+href); 
+//      System.out.println("dataDefault: "+dataDefault+ " --href: "+href); 
       uDefault = new URL(dataDefault);
       u = new URL(uDefault, href); 
     }
-    System.out.println("----URL:"+u.toString());
+//    System.out.println("----URL:"+u.toString());
     InputStream is = u.openStream(); 
     InputStreamReader isr = new InputStreamReader(is);
 
@@ -67,6 +117,10 @@ public class MyURIResolver implements URIResolver{
     }
   }
 
+  /*
+   * removes the DOCTYPE element and its contents from a Sting
+   * used to avoid problems with incorrect SystemIDs 
+   */
   private String removeDOCTYPE(String in) {  
     String ret = "";
     int startpos = in.indexOf("<!DOCTYPE");
