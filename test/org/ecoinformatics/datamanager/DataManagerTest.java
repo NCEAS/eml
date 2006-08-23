b67d1b010f6523d8eef8891fa6a31f8cd985d992
Duane Costa <dcosta@lternet.edu>
2006-08-23 21:54:11 +0000

    Add unit test for DataManager.parseMetadata() method. At this point, the test server and test document are hard-coded as constants. Perhaps later we should make these configurable properties.

--- test/org/ecoinformatics/datamanager/DataManagerTest.java
+++ test/org/ecoinformatics/datamanager/DataManagerTest.java
@@ -1,9 +1,15 @@
 package org.ecoinformatics.datamanager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.ecoinformatics.datamanager.parser.DataPackage;
 
 public class DataManagerTest extends TestCase {
   
@@ -15,6 +21,10 @@ public class DataManagerTest extends TestCase {
   /*
    * Instance fields
    */
+  
+  private DataManager dataManager;
+  private final String TEST_DOCUMENT = "knb-lter-mcm.7002.1";
+  private final String TEST_SERVER = "http://metacat.lternet.edu/knb/metacat";
   
   
   /*
@@ -43,6 +53,7 @@ public class DataManagerTest extends TestCase {
     TestSuite testSuite = new TestSuite();
     
     testSuite.addTest(new DataManagerTest("initialize"));
+    testSuite.addTest(new DataManagerTest("parseMetadata"));
     
     return testSuite;
   }
@@ -59,13 +70,55 @@ public class DataManagerTest extends TestCase {
   public void initialize() {
     assertTrue(1 == 1);
   }
+  
+
+  /**
+   * Unit test for the DataManager.parseMetadata() method (Use Case #1). 
+   * 
+   * @throws MalformedURLException
+   * @throws IOException
+   * @throws Exception
+   */
+  public void parseMetadata()
+        throws MalformedURLException, IOException, Exception {
+    DataPackage dataPackage = null;
+    InputStream metadataInputStream;
+    String documentURL = TEST_SERVER + 
+                         "?action=read&qformat=xml&docid=" + 
+                         TEST_DOCUMENT;
+    URL url;
+    
+    try {
+      url = new URL(documentURL);
+      metadataInputStream = url.openStream();
+      dataPackage = dataManager.parseMetadata(metadataInputStream);
+    }
+    catch (MalformedURLException e) {
+      e.printStackTrace();
+      throw(e);
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      throw(e);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      throw(e);
+    }
+
+    /*
+     * Assert that dataManager.parseMetadata() returned a non-null 
+     * dataPackage object.
+     */
+    assertNotNull("Data package is null", dataPackage);
+  }
 
   
   /**
    * Establish a testing framework by initializing appropriate objects.
    */
   public void setUp() {
-    
+    dataManager = DataManager.getInstance();
   }
   
   
@@ -73,7 +126,7 @@ public class DataManagerTest extends TestCase {
    * Release any objects after tests are complete.
    */
   public void tearDown() {
-    
+    dataManager = null;
   }
   
 }
