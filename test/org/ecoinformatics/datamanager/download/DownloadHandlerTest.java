dd6474b6521f43e91018953cdf13a3948d520e53
Jing Tao <tao@nceas.ucsb.edu>
2006-09-21 00:39:43 +0000

    Add test method for testing two handler has same url.

--- test/org/ecoinformatics/datamanager/download/DownloadHandlerTest.java
+++ test/org/ecoinformatics/datamanager/download/DownloadHandlerTest.java
@@ -115,6 +115,62 @@ public class DownloadHandlerTest extends TestCase
 	  }
 	  
 	  /**
+	   * Test two DownloadHandler with same url
+	   *
+	   */
+	  public void tesDownloadHandlerWithSameUrl()
+	  {
+		  String url = "http://knb.ecoinformatics.org/knb/metacat?action=read&qformat=xml&docid=adler.5.1";
+		  processDownloadHandlersWithSameUrl(url);
+	  }
+	  
+	  
+	  /*
+	   * Process two downloadhandler with same url
+	   */
+	  private void processDownloadHandlersWithSameUrl(String url)
+	  {
+		  
+		  DownloadHandler handler = DownloadHandler.getInstance(url);
+		  //System.out.println("here1");
+		  DataStorageTest dataStorage = new DataStorageTest();
+     	  DataStorageTest[] list = new DataStorageTest[1];
+		  list[0] = dataStorage;
+		  handler.setDataStorageCladdList(list);
+		  
+		  //System.out.println("here2");
+		  assertTrue(handler.isBusy() == false);
+		  assertTrue(handler.isSuccess() == false);
+		  Thread downloadThread = new Thread(handler);
+		  //System.out.println("here3");
+		  downloadThread.start();
+		  // start the second handler
+		  DownloadHandler handler2 = DownloadHandler.getInstance(url);
+		  handler2.setDataStorageCladdList(list);
+		  Thread downloadThread2 = new Thread(handler2);
+		  downloadThread2.start();
+		  //assertTrue(handler == handler2);
+		  System.out.println("the handler is "+handler);
+		  System.out.println("the handler2 is "+handler2);
+		  //System.out.println("here4");
+		  while(!handler.isCompleted())
+		  {
+			 
+		  }
+		  while(!handler2.isCompleted())
+		  {
+			 
+		  }
+		  System.out.println("the handler is ===="+handler);
+		  System.out.println("the handler2 is ===="+handler2);
+		  //assertTrue(handler.isSuccess() == true);
+		  assertTrue(dataStorage.doesDataExist(url) == true);
+	      assertTrue(handler.isSuccess() == true);
+		  assertTrue(handler.isBusy() == false);
+		  assertTrue(handler2.isSuccess() == true);
+		  assertTrue(handler2.isBusy() == false);
+	  }
+	  /**
 	   * Create a suite of tests to be run together
 	   */
 	   public static Test suite()
@@ -124,6 +180,7 @@ public class DownloadHandlerTest extends TestCase
 	     suite.addTest(new DownloadHandlerTest("testDownloadSuccess"));	 
 	     suite.addTest(new DownloadHandlerTest("testEcogridDownloadFailed"));
 	     suite.addTest(new DownloadHandlerTest("testEcogridDownloadSuccess"));
+	     suite.addTest(new DownloadHandlerTest("tesDownloadHandlerWithSameUrl"));
 	     return suite;
 	   }
 }
