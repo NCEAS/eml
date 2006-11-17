6d5b423949b7ab8b521df3b9b9995093ba8cf244
Jing Tao <tao@nceas.ucsb.edu>
2006-11-17 02:01:58 +0000

    Add more error messages.

--- src/org/ecoinformatics/datamanager/database/UnWellFormedQueryException.java
+++ src/org/ecoinformatics/datamanager/database/UnWellFormedQueryException.java
@@ -2,8 +2,8 @@
  *    '$RCSfile: UnWellFormedQueryException.java,v $'
  *
  *     '$Author: tao $'
- *       '$Date: 2006-11-16 21:35:08 $'
- *   '$Revision: 1.1 $'
+ *       '$Date: 2006-11-17 02:01:58 $'
+ *   '$Revision: 1.2 $'
  *
  *  For Details: http://kepler.ecoinformatics.org
  *
@@ -49,6 +49,14 @@ public class UnWellFormedQueryException extends Exception
 	public static final String CONDITION_VALUE_IS_NULL ="Value couldn't be null in condition";
 	public static final String CONDITOION_VALUE_IS_NOT_NUMBER = " is NOT a number in condition, but it should be because it use a numeric operator ";
 	public static final String CONDITION_NOT_HANDLED_OPERATOR = "Sorry, we couldn't handle this operator: ";
+	public static final String JOIN_ENTITY_IS_NULL = "The entity in join part couldn't be null";
+	public static final String JOIN_ATTRIBUTE_IS_NULL = "The attribute in join part couldn't be null";
+	public static final String JOIN_ENTITY_NAME_IS_NULL = "The entity name in joint part couldn't be null";
+	public static final String JOIN_ATTRIBUTE_NAME_IS_NULL = "The attribute name in join part couldn't be null";
+	public static final String LOGICALREALTION_IS_NULL = "There is no sub-compents in logical (AND or OR) relation";
+	public static final String LOGICALREALTION_HAS_ONE_SUBCOMPOENT = "There is only one sub components in logical relation";
+	public static final String WHERECLAUSE_IS_NULL = "There is no component in where clause";
+	public static final String QUERY_SELECTION_OR_TABLE_IS_NULL = "Selection item or From table item is null in query";
 	/**
     * Constructor
     * @param error  error message in this exception
