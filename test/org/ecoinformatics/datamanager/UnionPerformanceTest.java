4790725428be71f0ecfa24f6dd4eb022980d6f12
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2007-10-02 23:10:41 +0000

    test for performing unions on datasets with many datatables assumes a few things about the data, which isn't the most reusable, but it's a start

---
+++ test/org/ecoinformatics/datamanager/UnionPerformanceTest.java
@@ -0,0 +1,248 @@
+package org.ecoinformatics.datamanager;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.ResultSet;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ecoinformatics.datamanager.database.Condition;
+import org.ecoinformatics.datamanager.database.DatabaseConnectionPoolInterfaceTest;
+import org.ecoinformatics.datamanager.database.Query;
+import org.ecoinformatics.datamanager.database.SelectionItem;
+import org.ecoinformatics.datamanager.database.TableItem;
+import org.ecoinformatics.datamanager.database.Union;
+import org.ecoinformatics.datamanager.database.WhereClause;
+import org.ecoinformatics.datamanager.download.EcogridEndPointInterfaceTest;
+import org.ecoinformatics.datamanager.parser.Attribute;
+import org.ecoinformatics.datamanager.parser.AttributeList;
+import org.ecoinformatics.datamanager.parser.DataPackage;
+import org.ecoinformatics.datamanager.parser.Entity;
+
+public class UnionPerformanceTest extends TestCase {
+
+	/*
+	 * Class fields
+	 */
+	public Log log = LogFactory.getLog(UnionPerformanceTest.class);
+	
+	private final String QUERY_TEST_DOCUMENT = "leinfelder.5.8";
+	private final String QUERY_TEST_SERVER = "http://localhost:8080/knb/metacat";
+	//private final String QUERY_TEST_SERVER = "http://fred.msi.ucsb.edu:8080/knb/metacat";
+
+	
+	/*
+	 * Instance fields
+	 */
+	private DataManager dataManager;
+	private EcogridEndPointInterfaceTest endPointInfo = null;
+	private int conditionColumnIndex = 3;
+	
+	/*
+	 * Constructors
+	 */
+
+	/**
+	 * Because DataManagerTest is a subclass of TestCase, it must provide a
+	 * constructor with a String parameter.
+	 * 
+	 * @param name
+	 *            the name of a test method to run
+	 */
+	public UnionPerformanceTest(String name) {
+		super(name);
+	}
+
+	/*
+	 * Class methods
+	 */
+
+	/**
+	 * Create a suite of tests to be run together.
+	 */
+	public static Test suite() {
+		TestSuite testSuite = new TestSuite();
+
+		testSuite.addTest(new UnionPerformanceTest("initialize"));
+		testSuite.addTest(new UnionPerformanceTest("testSelectData"));
+
+		return testSuite;
+	}
+
+	/*
+	 * Instance methods
+	 */
+
+	/**
+	 * Run an initial test that always passes to check that the test harness is
+	 * working.
+	 */
+	public void initialize() {
+		assertTrue(1 == 1);
+	}
+
+	/**
+	 * Establish a testing framework by initializing appropriate objects.
+	 */
+	public void setUp() throws Exception {
+		super.setUp();
+		DatabaseConnectionPoolInterfaceTest connectionPool = new DatabaseConnectionPoolInterfaceTest();
+		String dbAdapterName = connectionPool.getDBAdapterName();
+		dataManager = DataManager.getInstance(connectionPool, dbAdapterName);
+		endPointInfo = new EcogridEndPointInterfaceTest();
+	}
+
+	/**
+	 * Release any objects after tests are complete.
+	 */
+	public void tearDown() throws Exception {
+		dataManager = null;
+		endPointInfo = null;
+		super.tearDown();
+	}
+
+	/**
+	 * Unit test for the DataManager.selectData() method. Tests the creation and
+	 * use of a Query object for querying a data table. Runs a query and checks
+	 * the result set against known values in the data table. This test is
+	 * essentially a complete cradle-to-grave test of Uses Cases 1-4.
+	 */
+	public void testSelectData() throws Exception {
+		
+		log.info("starting the test");
+		
+		long startTime = System.currentTimeMillis();
+		
+		Entity entity = null;
+		AttributeList attributeList;
+		Attribute attribute;
+		Attribute conditionAttribute;
+		DataPackage dataPackage = null;
+		DataPackage[] dataPackages = null;
+		Union union = null;
+		
+		String documentURL = 
+			QUERY_TEST_SERVER
+			+ "?action=read&qformat=xml&docid=" 
+			+ QUERY_TEST_DOCUMENT;
+		
+		InputStream inputStream = null;
+		String operator = ">";
+		boolean success;
+		Integer value = new Integer(990);
+		ResultSet resultSet = null;
+		URL url;
+
+		// First create the DataPackage object that will be used in the query.
+		url = new URL(documentURL);
+		inputStream = url.openStream();
+		log.debug("about to parse datapackage");
+		
+		dataPackage = dataManager.parseMetadata(inputStream);
+		log.debug("parsed datapackage!");
+		String testDbTable = DataManager.getDBTableName(dataPackage.getEntityList()[0]);
+		log.debug("testDbTable=" + testDbTable);
+		if (testDbTable == null ) {
+			//load the package
+			success = dataManager.loadDataToDB(dataPackage, endPointInfo);
+		}
+		else {
+			//don't try to get the data again
+			success = true;
+		}
+		
+		log.debug("loaded data to db");
+
+		union = new Union();
+
+		Entity[] entityList = dataPackage.getEntityList();
+		for (int i = 0; i < entityList.length; i++) {
+			entity = entityList[i];
+			attributeList = entity.getAttributeList();
+			Attribute[] attributes = attributeList.getAttributes();
+			attribute = attributes[0];
+			conditionAttribute = attributes[conditionColumnIndex];
+			
+			//build some queries from each entity
+			if (success && dataPackage != null) {
+				dataPackages = new DataPackage[1];
+				dataPackages[0] = dataPackage;
+				Query query = new Query();
+				/* SELECT clause */
+				SelectionItem selectionItem = 
+					new SelectionItem(entity,attribute);
+				query.addSelectionItem(selectionItem);
+				/* FROM clause */
+				TableItem tableItem = new TableItem(entity);
+				query.addTableItem(tableItem);
+				/* WHERE clause with condition */
+				Condition condition = 
+					new Condition(entity, conditionAttribute, operator, value);
+				WhereClause whereClause = new WhereClause(condition);
+				query.setWhereClause(whereClause);
+				
+				log.debug("Query SQL = " + query.toSQLString());
+
+				//add the query to the union
+				union.addQuery(query);
+				
+			}//dp success
+			
+		} //for loop
+		
+		log.info("Union Query SQL = " + union.toSQLString());
+
+		try {
+			//try to get the results
+			resultSet = dataManager.selectData(union, dataPackages);
+
+			if (resultSet != null) {
+
+				int j = 1;
+
+				while (resultSet.next()) {
+					int column0 = resultSet.getInt(1);
+					log.debug("resultSet[" + j + "], column0 =  " + column0);
+					j++;
+				}
+			} 
+			else {
+				throw new Exception("resultSet is null");
+			}
+			log.info("about to drop tables");
+			//dataManager.dropTables(dataPackage); // Clean-up test tables
+		} 
+		catch (Exception e) {
+			System.err.println("Exception: "
+					+ e.getMessage());
+			e.printStackTrace();
+			throw (e);
+		} 
+		finally {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			
+		}
+		
+		long endTime = System.currentTimeMillis();
+		
+		log.info("time to run in millis: " + (endTime - startTime));
+	}
+	
+	public static void main(String args[]) {
+		UnionPerformanceTest upt = new UnionPerformanceTest("UnionPerformanceTest");
+		try {
+			upt.setUp();
+			upt.testSelectData();
+			upt.tearDown();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
