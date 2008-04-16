4629ea3cc0875db744e15d03af1007dd0e1a0787
Margaret O'Brien <margaret.obrien@ucsb.edu>
2008-04-16 18:04:18 +0000

    updated all 2.1.0 namespaces to "rc2"

--- update_namespaces.sh
+++ update_namespaces.sh
@@ -2,13 +2,15 @@
 
 # script: update_namespaces.sh
 # a quick way to do several search-and-replace 
-# keep the infile intact by sending to standard out, redirect to new name, 
-# or redirect sed output to a tempfile here and replace original.
+# keep the infile intact by sending to standard out, redirect to new name. 
+# or redirect sed output to a tempfile here and replace original. Use xargs, or
+#	 for schema in `ls *xsd`; do ./update_namespaces.sh $schema; done
 
 infile=$1;
 
 
-sed    " { s/-2.0.1/-2.1.0rc1/g
+sed    " { s/-2.1.0rc1/-2.1.0rc2/g
+
 
 
 		} " < $infile  >tempfile
