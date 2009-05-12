feabea5a01b6caf360c1756719b9e226b32808b3
Matt Jones <jones@nceas.ucsb.edu>
2009-05-12 16:14:50 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_EML_UTILS_0_9_1'.

--- bin/xsltransform.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# usage: xslttransform file.xml style.xsl output.xml
-# A simple wrapper on xalan to run an XSL transform
-#
-LIB=../lib/apache
-PARSER=$LIB/xalan.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar
-java -cp $PARSER org.apache.xalan.xslt.Process -IN $1 -XSL $2 -OUT $3
