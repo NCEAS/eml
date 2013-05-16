bbe18b9a2df2b7cadcd330e936b36ca1a51d7495
Duane Costa <dcosta@lternet.edu>
2013-05-16 17:43:30 +0000

    Bug #4393: 'Use datamanager for EML QA/QC'.   - Strip non-valid XML characters out of the quality report XML document.

--- src/org/ecoinformatics/datamanager/quality/QualityReport.java
+++ src/org/ecoinformatics/datamanager/quality/QualityReport.java
@@ -547,8 +547,37 @@ public class QualityReport {
 
     stringBuffer.append("</qr:qualityReport>\n");
     xmlString = stringBuffer.toString();
-    
+    xmlString = stripNonValidXMLCharacters(xmlString);
     return xmlString;
+  }
+  
+  
+  /**
+   * This method ensures that the output String has only valid XML unicode characters as 
+   * specified by the XML 1.0 standard. For reference, please see 
+   * <a href="http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char">the standard</a>. 
+   * This method will return an empty String if the input is null or empty.
+   *
+   * @param in The String whose non-valid characters we want to remove.
+   * @return The in String, stripped of non-valid characters.
+   */
+  private String stripNonValidXMLCharacters(String in) {
+      StringBuffer out = new StringBuffer(); // Used to hold the output.
+      char current; // Used to reference the current character.
+
+      if (in == null || ("".equals(in))) return ""; // vacancy test.
+      for (int i = 0; i < in.length(); i++) {
+          current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+          if ((current == 0x9) ||
+              (current == 0xA) ||
+              (current == 0xD) ||
+              ((current >= 0x20) && (current <= 0xD7FF)) ||
+              ((current >= 0xE000) && (current <= 0xFFFD)) ||
+              ((current >= 0x10000) && (current <= 0x10FFFF))) {
+              out.append(current);
+          }
+      }
+      return out.toString();
   }
 
 }
