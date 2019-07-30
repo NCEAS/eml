808fb1f96ac1d7c4dd9bde8d8b4a6be3f87f4af4
Bryce Mecum <petridish@gmail.com>
2019-07-29 21:08:03 -0800

    Add Oxygen docs to book and tweak the book build
    I built the Oxygen docs with our build script and commit them here and
    also integrate them into the Bookdown book with a custom build script.
    
    The new build script does two things:
    
      1. Renames the HTML output of ../README.md which we want to use as
         the index.html of the book. A custom build script has to do this
         because Bookdown tries to use the top level header of the file
         as the filename but we want to use index.html so the book works
         nicely.
    
      2. Copies in the Oxygen docs into the build book

---
+++ docs/build_book.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+# We customized the build process so we could use the README.md from the root
+# of the repo as the index page for the entire book, rather than duplicating
+# the content or writing new content.
+
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+
+# Rename the HTML Bookdown produces from the ../README.md file to index.html
+file.rename("_book/eml-ecological-metadata-language.html", "_book/index.html")
+
+# Copy the Oxygen XML schema docs into the book
+system(paste("cp -r", "./schema", "./_book/schema"))
