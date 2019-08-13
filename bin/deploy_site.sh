3704c5365343eb8cb602ac8c5e70a1eab07f13bb
Matt Jones <gitcode@magisa.org>
2019-08-13 08:58:36 -0700

    Deploy from the top level dist dir.

--- bin/deploy_site.sh
+++ bin/deploy_site.sh
@@ -7,5 +7,5 @@
 # 1. delete the current gh_pages branch
 # 2. push the current docs/dist directory to gh_pages
 
-git push origin :gh-pages && git subtree push --prefix docs/dist origin gh-pages
+git push origin :gh-pages && git subtree push --prefix dist origin gh-pages
 
