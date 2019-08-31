ee2567e31fc6a4ec6cb7552423df896a5146eff1
Matt Jones <gitcode@magisa.org>
2019-08-30 18:04:05 -0800

    Update the deploy script.
    THe script now builds the distribution site from scratch by running
    the build_book.R script, and then copies source files like images and
    schema docs and xsd files into the dist directory.  The dist directory
    is then added to a temporary branch, and pushed to gh_pages to be picked
    up by Netlify.  The temporary branch is then deleted, and we switch back
    to the branch from which the original docs were built.

--- bin/deploy_site.sh
+++ bin/deploy_site.sh
@@ -3,9 +3,32 @@
 # usage: ./bin/deploy_site.sh
 # run script from the top of EML checkout
  
-# actions:
-# 1. delete the current gh_pages branch
-# 2. push the current docs/dist directory to gh_pages
+# 1. create a temporary branch for the dist files
+CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
+TEMP_BRANCH=dist_$$
+echo "CURRENT: ${CURRENT_BRANCH}"
+echo "   TEMP: ${TEMP_BRANCH}"
+git checkout -b ${TEMP_BRANCH}
 
+# 2. build the Bookdown book into the dist directory
+cd docs
+R -f 'build_book.R'
+cd ..
+
+# 3. Add and commit the build site to dist on the temp branch
+git add dist
+git commit -m "Deploy site to netlify"
+git status
+
+# 4. delete the current gh_pages branch
+# 5. push the temp dist directory to origin gh_pages
 git push origin :gh-pages && git subtree push --prefix dist origin gh-pages
 
+# 6. switch back to the original branch
+git checkout ${CURRENT_BRANCH}
+
+# 7. delete the temporary branch
+git branch -l
+git branch -D ${TEMP_BRANCH}
+
+echo "Site deployed. Check https://eml.ecoinformatics.org "
